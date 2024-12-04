863d60b3adc3480271547edb240ef2228d9fdc7d
Callum Terry <callumlrt@gmail.com>
2024-12-04 02:20:18 +0000

    Day 3 – Visual Basic: re-writing to work on Mac using dotnet

--- 2024/03/Solution.vb
+++ 2024/03/Solution.vb
@@ -1,29 +1,33 @@
+Imports System.IO
 Imports System.Text.RegularExpressions
 
-' Code developed using https://onecompiler.com/vb/42zxh5cne
-
 Public Module Program
-   Sub calculateMatches(text As String, expr As String)
-      Dim mc As MatchCollection = Regex.Matches(text, expr, RegexOptions.Multiline)
-      Dim m As Match
-      Dim total As Integer
-      Dim matches As Integer = 0
-      
-      For Each m In mc
-        Dim lv As Integer = Convert.toInt32(m.Groups(1).Value)
-        Dim rv As Integer = Convert.toInt32(m.Groups(2).Value)
-        total = total + lv * rv
-      Next m
-      
-      Console.WriteLine(total)
-   End Sub
-   
-  Sub solution1(name As String)
-    calculateMatches(name, "mul\((\d{1,3}),(\d{1,3})\)")
-  End Sub
+    Private Function calculateLineMatches(text As String, expr As String) As Integer
+        Dim mc As MatchCollection = Regex.Matches(text, expr, RegexOptions.Multiline)
+        Dim m As Match
+        Dim lineTotal As Integer
+        Dim matches As Integer = 0
+        
+        For Each m In mc
+            Dim leftVal As Integer = Convert.toInt32(m.Groups(1).Value)
+            Dim rightVal As Integer = Convert.toInt32(m.Groups(2).Value)
+            lineTotal = lineTotal + leftVal * rightVal
+        Next m
+        
+        Return lineTotal
+    End Function
+    
+    Private Function solution1() As Integer
+        Dim total As Integer
+        
+        For Each line As String In File.ReadLines("input.txt")
+            total += calculateLineMatches(line, "mul\((\d{1,3}),(\d{1,3})\)")
+        Next line
+
+        Return total
+    End Function
   
-	Public Sub Main(args() As string)
-	  Dim input as String = Console.ReadLine()
-	  solution1(input)
-	End Sub
+    Public Sub Main(args() As string)
+        Console.WriteLine(solution1())
+    End Sub
 End Module
