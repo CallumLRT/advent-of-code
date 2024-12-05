a222d3b7042c087bb80cd31d35872a60393ebbba
Callum Terry <callumlrt@gmail.com>
2024-12-05 00:48:18 +0000

    Day 3 – Visual Basic (.NET): solution 2 complete

--- 2024/03/Solution.vb
+++ 2024/03/Solution.vb
@@ -2,32 +2,55 @@ Imports System.IO
 Imports System.Text.RegularExpressions
 
 Public Module Program
-    Private Function calculateLineMatches(text As String, expr As String) As Integer
-        Dim mc As MatchCollection = Regex.Matches(text, expr, RegexOptions.Multiline)
+    Private Function calculateMatches(text As String) As Integer
+        Dim mc As MatchCollection = Regex.Matches(text, "mul\((\d{1,3}),(\d{1,3})\)")
         Dim m As Match
-        Dim lineTotal As Integer
-        Dim matches As Integer = 0
+        Dim total As Integer
         
         For Each m In mc
             Dim leftVal As Integer = Convert.toInt32(m.Groups(1).Value)
             Dim rightVal As Integer = Convert.toInt32(m.Groups(2).Value)
-            lineTotal = lineTotal + leftVal * rightVal
+            total += + leftVal * rightVal
         Next m
         
-        Return lineTotal
+        Return total
     End Function
+
+    Private Sub getInput(byRef text As String)
+        For Each line As String In File.ReadLines("input.txt")
+            text += line
+        Next line
+    End Sub
     
     Private Function solution1() As Integer
+        Dim fullInput As String = ""
+        getInput(fullInput)
+
+        Return calculateMatches(fullInput)
+    End Function
+
+    Private Function findValidBlocks(text As String) As Integer
+        Dim mc As MatchCollection = Regex.Matches(text, "do\(\)(.*?)don't\(\)")
+        Dim m As Match
         Dim total As Integer
         
-        For Each line As String In File.ReadLines("input.txt")
-            total += calculateLineMatches(line, "mul\((\d{1,3}),(\d{1,3})\)")
-        Next line
-
+        For Each m In mc
+            total += calculateMatches(m.Value)
+        Next m
+        
         Return total
+    End Function
+
+    Private Function solution2() As String
+        Dim fullInput As String = "do()"
+        getInput(fullInput)
+        fullInput = fullInput + "don't()"
+
+        Return findValidBlocks(fullInput)
     End Function
   
     Public Sub Main(args() As string)
         Console.WriteLine(solution1())
+        Console.WriteLine(solution2())
     End Sub
 End Module
